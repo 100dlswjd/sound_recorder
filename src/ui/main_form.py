3e12fa76722f1e404d4f05cd4ffe23964ff418fb
100dlswjd <100dlswjd@naver.com>
2022-08-13 23:20:18 +0900

    유아이 수정

--- src/ui/main_form.py
+++ src/ui/main_form.py
@@ -15,27 +15,41 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(320, 234)
+        MainWindow.resize(200, 123)
+        MainWindow.setMaximumSize(QSize(200, 123))
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.btn_record = QPushButton(self.centralwidget)
-        self.btn_record.setObjectName(u"btn_record")
-        self.btn_record.setGeometry(QRect(110, 100, 75, 24))
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(4, 4, 4, 4)
+        self.btn_recorde = QPushButton(self.centralwidget)
+        self.btn_recorde.setObjectName(u"btn_recorde")
+
+        self.verticalLayout.addWidget(self.btn_recorde)
+
         self.btn_play = QPushButton(self.centralwidget)
         self.btn_play.setObjectName(u"btn_play")
         self.btn_play.setEnabled(False)
-        self.btn_play.setGeometry(QRect(110, 150, 75, 24))
+
+        self.verticalLayout.addWidget(self.btn_play)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 320, 22))
+        self.menubar.setGeometry(QRect(0, 0, 200, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -47,8 +61,8 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.btn_record.setText(QCoreApplication.translate("MainWindow", u"\u25b6", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\ub179\uc74c\uae30", None))
+        self.btn_recorde.setText(QCoreApplication.translate("MainWindow", u"\u25b6", None))
         self.btn_play.setText(QCoreApplication.translate("MainWindow", u"\uc7ac\uc0dd", None))
     # retranslateUi
 
